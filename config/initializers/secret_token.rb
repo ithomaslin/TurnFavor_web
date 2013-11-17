ea8a7f77431a3e295334f2ba95e88709aef858ed
ithomaslin <ithomas.lin@me.com>
2013-11-17 00:51:34 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turnfavor::Application.config.secret_key_base = '125da26390723f69a983936f40dcecf32cde0c7942971f6a18cfb9a03c32a59aa6a3fd465d4d144aad9d56f761326c49a908684fdf59795f62435df5fb627866'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turnfavor::Application.config.session_store :cookie_store, key: '_turnfavor_session'
