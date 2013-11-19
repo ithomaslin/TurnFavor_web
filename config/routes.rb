d0fba00e48b95e1962304e17f8332393dfbd9669
ithomaslin <ithomas.lin@me.com>
2013-11-19 00:16:21 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Turnfavor::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
