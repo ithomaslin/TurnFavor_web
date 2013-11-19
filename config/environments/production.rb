59639f18696026ce765ccaef5bd5bf88d4e233cc
ithomaslin <ithomas.lin@me.com>
2013-11-19 14:17:28 +0000

    Implement devise and users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Turnfavor::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://secret-waters-5452.herokuapp.com/' }
 end
